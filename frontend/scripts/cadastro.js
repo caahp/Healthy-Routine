69ee84eef7170d4c4b24d27aedf64095d52b4a54
Caroline Pereira <carolline.ps@hotmail.com>
2023-03-28 23:33:20 -0400

    frontend working

--- frontend/scripts/cadastro.js
+++ frontend/scripts/cadastro.js
@@ -1,8 +1,17 @@
 const togglePassword = document.querySelector('.password-toggle');
 const password = document.querySelector('#password');
-	  
-	togglePassword.addEventListener('click', function(e) {
-		const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-		password.setAttribute('type', type);
-		this.querySelector('i').classList.toggle('fa-eye-slash');
-	});
+
+togglePassword.addEventListener('click', function(e) {
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  this.querySelector('i').classList.toggle('fa-eye-slash');
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // previne o comportamento padrão do formulário
+  alert('Cliente cadastrado!'); // exibe o pop-up
+  window.location.replace('individual.html'); // redireciona para a nova página
+  form.reset(); // limpa o formulário
+});
+
